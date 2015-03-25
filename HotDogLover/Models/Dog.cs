f4c30a4b122f40995b508081280b5d1036145fd9
arfriz01 <arfrizz@gmail.com>
2015-03-25 16:04:08 -0400

    first commit

---
+++ HotDogLover/Models/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotDogLover.Models
+{
+    public class Dog
+    {
+        public int HotdogID { get; set; }
+        public string Name { get; set; }
+        public DateTime LastAte { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
---
+++ HotDogLover/Models/Lover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotDogLover.Models
+{
+    public class Lover
+    {
+        public int LoverID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Biography { get; set; }
+        public string Image { get; set; }
+    }
+}
