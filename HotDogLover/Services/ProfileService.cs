1e7edd2c90ea05042b33bf258a5e912788fb0dee
arfriz01 <arfrizz@gmail.com>
2015-04-01 16:37:09 -0400

    changed favorites

--- HotDogLover/Services/ProfileService.cs
+++ HotDogLover/Services/ProfileService.cs
@@ -86,7 +86,7 @@ namespace HotDogLover.Services
                 Bio = "My name is Oliver Queen. For five years I was stranded on an island with only one goal - survive so that I could find the best Hotdog out there... When I returned to the city, I immediately began working on opening a HotDog Joint. I called it Ollie's Trollie. I experimented with all kinds of dogs and invite suggestions from my loyal customers, but I'm just not sure I have found the best dog. I would have to say the Cheese Filled Dog is the closest to perfection I have found thus far.",
                 Picture = "http://img3.wikia.nocookie.net/__cb20121109062657/arrow-france/fr/images/8/8f/Arrow_LesPersonnages_OliverQueen.jpg",
                 ProfileID = 1,
-                FavoriteHotDog = hotDogService.Get(1),
+                FavoriteHotDog = hotDogService.Get(2),
                 HotDogList = myFavs
             };
             profiles.Add(p1);
@@ -101,7 +101,7 @@ namespace HotDogLover.Services
                 Bio = "Hi, you can call me Fe! I am a totally nerd, but I do appreciate a good dog whenever I can get myself out of the lair. I don't like to leave often so I haven't been able to try many hotdogs yet. So far, I'd say my favorite is the Chilli Cheese Dog!",
                 Picture = "https://c2.staticflickr.com/8/7371/13039510383_f865f217f3_b.jpg",
                 ProfileID = 2,
-                FavoriteHotDog = hotDogService.Get(3),
+                FavoriteHotDog = hotDogService.Get(1),
                 HotDogList = myFavs
             };
             profiles.Add(p2);
@@ -115,7 +115,7 @@ namespace HotDogLover.Services
                 Bio = "I'm just in it for the dogs",
                 Picture = "http://img2.wikia.nocookie.net/__cb20131031025638/marvel_dc/images/5/55/Sara_Lance_(Arrow)_002.jpg",
                 ProfileID = 3,
-                FavoriteHotDog = hotDogService.Get(2),
+                FavoriteHotDog = hotDogService.Get(3),
                 HotDogList = myFavs
             };
             profiles.Add(p3);
